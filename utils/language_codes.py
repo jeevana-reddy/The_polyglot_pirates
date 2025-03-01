258b48363466d68e514b492c5dc85b629c605c4a
Jeevana Bhavani <154445471+jeevana-reddy@users.noreply.github.com>
2025-03-01 11:34:25 +0530

    Add files via upload

---
+++ utils/language_codes.py
@@ -0,0 +1,12 @@
+LANGUAGES = {
+    "English": "en",
+    "Spanish": "es",
+    "French": "fr",
+    "German": "de",
+    "Chinese": "zh",
+    "Hindi": "hi",
+    "Portuguese": "pt",
+    "Japanese": "ja",
+    "Dutch": "nl",
+    "Italian": "it",
+}
